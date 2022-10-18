4c216ac14bf09b95383f5d567501e60dbfd0f431
PrinceSP <princepasombaran@gmail.com>
2022-10-19 07:58:21 +0800

    using useRef hook for changing value

--- konsultasijo/src/pages/SignIn.js
+++ konsultasijo/src/pages/SignIn.js
@@ -1,16 +1,21 @@
-import React,{useState} from "react";
-import { getDatabase, ref, onValue } from "firebase/database";
+import React,{useState,useRef} from "react";
+import { getDatabase, ref, child, get } from "firebase/database";
 
 const SignIn = () => {
-  const [username,setUsername] = useState('')
-  const [password,setPassword] = useState('')
+  const username = useRef('')
+  const password = useRef('')
 
   function writeUserData( name, password) {
-    const db = getDatabase();
-    const starCountRef = ref(db, 'users/');
-    onValue(starCountRef, (snapshot) => {
-      const data = snapshot.val();
-      updateStarCount(postElement, data);
+    const dbRef = ref(getDatabase());
+    get(child(dbRef, `users/`))
+    .then((snapshot) => {
+      if (snapshot.exists()) {
+        console.log(snapshot.val());
+      } else {
+        console.log("No data available");
+      }
+    }).catch((error) => {
+      console.error(error);
     });
   }
 
@@ -19,9 +24,9 @@ const SignIn = () => {
         <h1>MASUK</h1>
         <form action="" style={{display:'flex',flexDirection:'column',width:"30%"}} className="mt-5">
           <label>Username</label>
-          <input style={{borderRadius:5,border:'1px solid #000',height:38}} className="mb-3" name="Username" value={username} type="text" onChange={(value)=>setUsername(value)}/>
+          <input style={{borderRadius:5,border:'1px solid #000',height:38}} className="mb-3" name="Username" ref={username} type="text" required/>
           <label>Password</label>
-          <input style={{borderRadius:5,border:'1px solid #000',height:38}} className="mb-5" name="Password" value={password} type="text" onChange={(value)=>setPassword(value)}/>
+          <input style={{borderRadius:5,border:'1px solid #000',height:38}} className="mb-5" name="Password" ref={password} type="text" required/>
           <button type="button" style={{width:"100%",height:38,border:'none',borderRadius:5,backgroundColor:"#000",color:"#fff"}}>Masuk</button>
         </form>
       </div>
