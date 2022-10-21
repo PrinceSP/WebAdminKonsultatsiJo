d040965d2a67c00f6f4f2ed074f7b0bc9533f602
PrinceSP <princepasombaran@gmail.com>
2022-10-21 17:30:51 +0800

    delete file input

--- konsultasijo/src/pages/News.js
+++ konsultasijo/src/pages/News.js
@@ -6,90 +6,74 @@ import ImgDeleteUser from '../assets/deleteUser.svg';
 
 const split={
     display: 'flex',
-    flexDirection: 'row',   
+    flexDirection: 'row',
 }
 const News = () => {
     return(
-        <>                               
-            <div style={split}>                            
-                <Navigation/>    
+        <>
+            <div style={split}>
+                <Navigation/>
                 <div className="gap">
-                
+
                 {/* Masukan Judul */}
                 <div class="input-group flex-nowrap mb-2" style={{display: 'flex',}}>
-                <input type="text" class="form-control" placeholder="Masukan Judul"/>                
+                <input type="text" class="form-control" placeholder="Masukan Judul"/>
                 </div>
 
                 {/* Masukan Link */}
                 <div class="input-group flex-nowrap mb-2">
-                <input type="text" class="form-control" placeholder="Masukan Link"/>                
+                <input type="text" class="form-control" placeholder="Masukan Link"/>
                 </div>
-
-                <div class="input-group flex-nowrap mb-2">
-                <input type="file" class="form-control" placeholder="Masukan Gambar"/>                
-                </div>
-                {/* Masukan Gambar   */}
 
                 <div class="col-auto">
                     <button type="submit" class="btn btn-danger mb-3 form-control">Posting</button>
                 </div>
-                
-                {/* <div className="newsWrapper">
-                    
-                    <SplitPane 
-                    split="vertical"
-                    minSize={560}
-                    >
-                    <p>terjadi pembantaian pes di universitas klabat, diduga karena dendam dihina terus menerus</p>
-                    <img src="https://akcdn.detik.net.id/community/media/visual/2022/10/05/nasib-brigjen-hendra-dari-berseragam-polri-hingga-rompi-tahanan-1_169.jpeg?w=700&q=90" alt="News" />
-                    </SplitPane>
-                                    
-                </div>                                 */}
+
                 <div className="wrapper">
-                    <div className="newsWrapper" style={{display: 'flex'}}>                                        
+                    <div className="newsWrapper" style={{display: 'flex'}}>
                         <p>terjadi pembantaian pes di universitas klabat, diduga karena dendam dihina terus menerus</p>
-                        <img className="imgberita" src="https://akcdn.detik.net.id/community/media/visual/2022/10/05/nasib-brigjen-hendra-dari-berseragam-polri-hingga-rompi-tahanan-1_169.jpeg?w=700&q=90" alt="News" />                                                        
+                        <img className="imgberita" src="https://akcdn.detik.net.id/community/media/visual/2022/10/05/nasib-brigjen-hendra-dari-berseragam-polri-hingga-rompi-tahanan-1_169.jpeg?w=700&q=90" alt="News" />
                         <img className="imgbutton" style={{}} type="button" onClick={()=> alert('testnews')} src={ImgDeleteUser} alt="DeleteAccount" />
                     </div>
 
-                    <div className="newsWrapper" style={{display: 'flex'}}>                                        
+                    <div className="newsWrapper" style={{display: 'flex'}}>
                         <p>terjadi pembantaian pes di universitas klabat, diduga karena dendam dihina terus menerus</p>
-                        <img className="imgberita" src="https://akcdn.detik.net.id/community/media/visual/2022/10/05/nasib-brigjen-hendra-dari-berseragam-polri-hingga-rompi-tahanan-1_169.jpeg?w=700&q=90" alt="News" />                                                        
-                        <img className="imgbutton" style={{}} type="button" onClick={()=> alert('testnews')} src={ImgDeleteUser} alt="DeleteAccount" />
-                    </div>    
-
-                    <div className="newsWrapper" style={{display: 'flex'}}>                                        
-                        <p>terjadi pembantaian pes di universitas klabat, diduga karena dendam dihina terus menerus</p>
-                        <img className="imgberita" src="https://akcdn.detik.net.id/community/media/visual/2022/10/05/nasib-brigjen-hendra-dari-berseragam-polri-hingga-rompi-tahanan-1_169.jpeg?w=700&q=90" alt="News" />                                                        
+                        <img className="imgberita" src="https://akcdn.detik.net.id/community/media/visual/2022/10/05/nasib-brigjen-hendra-dari-berseragam-polri-hingga-rompi-tahanan-1_169.jpeg?w=700&q=90" alt="News" />
                         <img className="imgbutton" style={{}} type="button" onClick={()=> alert('testnews')} src={ImgDeleteUser} alt="DeleteAccount" />
                     </div>
 
-                    <div className="newsWrapper" style={{display: 'flex'}}>                                        
+                    <div className="newsWrapper" style={{display: 'flex'}}>
                         <p>terjadi pembantaian pes di universitas klabat, diduga karena dendam dihina terus menerus</p>
-                        <img className="imgberita" src="https://akcdn.detik.net.id/community/media/visual/2022/10/05/nasib-brigjen-hendra-dari-berseragam-polri-hingga-rompi-tahanan-1_169.jpeg?w=700&q=90" alt="News" />                                                        
-                        <img className="imgbutton" style={{}} type="button" onClick={()=> alert('testnews')} src={ImgDeleteUser} alt="DeleteAccount" />
-                    </div>
-                    
-                    <div className="newsWrapper" style={{display: 'flex'}}>                                        
-                        <p>terjadi pembantaian pes di universitas klabat, diduga karena dendam dihina terus menerus</p>
-                        <img className="imgberita" src="https://akcdn.detik.net.id/community/media/visual/2022/10/05/nasib-brigjen-hendra-dari-berseragam-polri-hingga-rompi-tahanan-1_169.jpeg?w=700&q=90" alt="News" />                                                        
+                        <img className="imgberita" src="https://akcdn.detik.net.id/community/media/visual/2022/10/05/nasib-brigjen-hendra-dari-berseragam-polri-hingga-rompi-tahanan-1_169.jpeg?w=700&q=90" alt="News" />
                         <img className="imgbutton" style={{}} type="button" onClick={()=> alert('testnews')} src={ImgDeleteUser} alt="DeleteAccount" />
                     </div>
 
-                    <div className="newsWrapper" style={{display: 'flex'}}>                                        
+                    <div className="newsWrapper" style={{display: 'flex'}}>
                         <p>terjadi pembantaian pes di universitas klabat, diduga karena dendam dihina terus menerus</p>
-                        <img className="imgberita" src="https://akcdn.detik.net.id/community/media/visual/2022/10/05/nasib-brigjen-hendra-dari-berseragam-polri-hingga-rompi-tahanan-1_169.jpeg?w=700&q=90" alt="News" />                                                        
+                        <img className="imgberita" src="https://akcdn.detik.net.id/community/media/visual/2022/10/05/nasib-brigjen-hendra-dari-berseragam-polri-hingga-rompi-tahanan-1_169.jpeg?w=700&q=90" alt="News" />
                         <img className="imgbutton" style={{}} type="button" onClick={()=> alert('testnews')} src={ImgDeleteUser} alt="DeleteAccount" />
                     </div>
 
-                    <div className="newsWrapper" style={{display: 'flex'}}>                                        
+                    <div className="newsWrapper" style={{display: 'flex'}}>
                         <p>terjadi pembantaian pes di universitas klabat, diduga karena dendam dihina terus menerus</p>
-                        <img className="imgberita" src="https://akcdn.detik.net.id/community/media/visual/2022/10/05/nasib-brigjen-hendra-dari-berseragam-polri-hingga-rompi-tahanan-1_169.jpeg?w=700&q=90" alt="News" />                                                        
+                        <img className="imgberita" src="https://akcdn.detik.net.id/community/media/visual/2022/10/05/nasib-brigjen-hendra-dari-berseragam-polri-hingga-rompi-tahanan-1_169.jpeg?w=700&q=90" alt="News" />
                         <img className="imgbutton" style={{}} type="button" onClick={()=> alert('testnews')} src={ImgDeleteUser} alt="DeleteAccount" />
-                    </div>    
-                </div>                                                                  
-            </div>      
-            </div>                      
+                    </div>
+
+                    <div className="newsWrapper" style={{display: 'flex'}}>
+                        <p>terjadi pembantaian pes di universitas klabat, diduga karena dendam dihina terus menerus</p>
+                        <img className="imgberita" src="https://akcdn.detik.net.id/community/media/visual/2022/10/05/nasib-brigjen-hendra-dari-berseragam-polri-hingga-rompi-tahanan-1_169.jpeg?w=700&q=90" alt="News" />
+                        <img className="imgbutton" style={{}} type="button" onClick={()=> alert('testnews')} src={ImgDeleteUser} alt="DeleteAccount" />
+                    </div>
+
+                    <div className="newsWrapper" style={{display: 'flex'}}>
+                        <p>terjadi pembantaian pes di universitas klabat, diduga karena dendam dihina terus menerus</p>
+                        <img className="imgberita" src="https://akcdn.detik.net.id/community/media/visual/2022/10/05/nasib-brigjen-hendra-dari-berseragam-polri-hingga-rompi-tahanan-1_169.jpeg?w=700&q=90" alt="News" />
+                        <img className="imgbutton" style={{}} type="button" onClick={()=> alert('testnews')} src={ImgDeleteUser} alt="DeleteAccount" />
+                    </div>
+                </div>
+            </div>
+            </div>
         </>
     );
 }
